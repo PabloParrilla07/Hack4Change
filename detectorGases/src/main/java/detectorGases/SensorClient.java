66370ce1fc2bfae2d1371536614d19e70427b482
PabloParrilla07 <pabparche@alum.us.es>
2025-04-14 12:15:05 +0200

    mqttStart
    He metido el mqttClient dentro del ServerClient. Si bien tengo entendido creo que el client es solo para iniciar? Y tanto mqtt como rest client hay que iniciarlos. Si los ponemos en distintos archivos solo podemos iniciar 1 de los 2

--- detectorGases/src/main/java/detectorGases/SensorClient.java
+++ detectorGases/src/main/java/detectorGases/SensorClient.java
@@ -1,20 +1,53 @@
 package detectorGases;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Stream;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 import detectorGases.entidades.Sensor;
+import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.mqtt.MqttClient;
+import io.vertx.mqtt.MqttClientOptions;
 
 public class SensorClient extends AbstractVerticle{
 	
 	public SensorClientUtil SensorClientUtil;
+	Gson gson;
 
 	public void start(Promise<Void> startFuture) {
+		
+		gson = new Gson();
+		MqttClient mqttClient = MqttClient.create(vertx, new MqttClientOptions().setAutoKeepAlive(true));
+		mqttClient.connect(1883, "localhost", s -> {
+		
+			mqttClient.subscribe("topic_2", MqttQoS.AT_LEAST_ONCE.value(), handler -> {
+				if (handler.succeeded()) {
+					System.out.println("Suscripción " + mqttClient.clientId());
+				}
+			});
+
+			mqttClient.publishHandler(handler -> {
+				System.out.println("Mensaje recibido:");
+				System.out.println("    Topic: " + handler.topicName().toString());
+				System.out.println("    Id del mensaje: " + handler.messageId());
+				System.out.println("    Contenido: " + handler.payload().toString());
+				try {
+				Sensor sc = gson.fromJson(handler.payload().toString(), Sensor.class);
+				System.out.println("    SimpleClass: " + sc.toString());
+				}catch (JsonSyntaxException e) {
+					System.out.println("    No es una SimpleClass. ");
+				}
+			});
+			mqttClient.publish("topic_6", Buffer.buffer("Rico mango"), MqttQoS.AT_LEAST_ONCE, false, false);
+			startFuture.complete();
+		});
+		
 		WebClientOptions options = new WebClientOptions().setUserAgent("RestClientApp/2.0.2.1");
 		options.setKeepAlive(false);
 		SensorClientUtil = new SensorClientUtil(WebClient.create(vertx, options));
